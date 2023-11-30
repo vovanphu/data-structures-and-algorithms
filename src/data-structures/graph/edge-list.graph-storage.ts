46aba6feedc7fb5a1d6788616b077f8e0c36034d
vovanphu <vovanphu1012@gmail.com>
2023-11-30 14:02:30 +0000

    Implement EdgeList_GraphStorage

---
+++ src/data-structures/graph/edge-list.graph-storage.ts
@@ -0,0 +1,90 @@
+import { GraphStorage_Interface } from '@root/data-structures';
+
+export class Edge {
+  public source: number;
+  public destination: number;
+  public weight?: number | undefined;
+
+  constructor(source: number, destination: number, weight: number = 0) {
+    this.source = source;
+    this.destination = destination;
+    this.weight = weight;
+  }
+}
+
+export class EdgeList_GraphStorage implements GraphStorage_Interface {
+  private vertices: number;
+  private edges: Array<Edge>;
+
+  constructor(size?: number) {
+    this.vertices = 0;
+    this.edges = new Array<Edge>();
+
+    if (size !== undefined) {
+      this.vertices = size as number;
+    }
+  }
+
+  size(): number {
+    return this.vertices;
+  }
+
+  setVertex(vertex: number): void {
+    if (vertex < 0) {
+      throw new Error('Negative vertex is not allowed');
+    }
+
+    this.vertices = Math.max(this.vertices, vertex + 1);
+  }
+
+  setEdge(
+    source: number,
+    destination: number,
+    weight?: number | undefined,
+  ): void {
+    if (source < 0 || destination < 0) {
+      throw new Error('Negative vertex is not allowed');
+    }
+
+    weight = weight || 0;
+    const newEdge = new Edge(source, destination, weight);
+    const existEdgeIndex = this.edges.findIndex(
+      (edge) => edge.source === source && edge.destination === destination,
+    );
+
+    if (existEdgeIndex > -1) {
+      this.edges[existEdgeIndex].weight = weight;
+    } else {
+      this.edges.push(newEdge);
+    }
+
+    this.vertices = Math.max(this.vertices, source + 1, destination + 1);
+  }
+
+  weight(source: number, destination: number): number {
+    const lastIndex = this.size() - 1;
+
+    if (source > lastIndex || destination > lastIndex) {
+      throw new Error('Out of graph bound');
+    }
+
+    const existEdge = this.edges.find(
+      (edge) => edge.source === source && edge.destination === destination,
+    );
+
+    if (existEdge === undefined) return Infinity;
+    return existEdge.weight as number;
+  }
+
+  neighbors(vertex: number): number[] {
+    const lastIndex = this.size() - 1;
+
+    if (vertex > lastIndex) {
+      throw new Error('Out of graph bound');
+    }
+
+    return this.edges
+      .filter((edge) => edge.source === vertex)
+      .map((edge) => edge.destination);
+  }
+}
--- src/data-structures/graph/index.ts
+++ src/data-structures/graph/index.ts
@@ -1,4 +1,5 @@
 export * from './adjacency-list.graph-storage';
 export * from './adjacency-matrix.graph-storage';
+export * from './edge-list.graph-storage';
 export * from './graph';
 export * from './graph-storage.inferface';
