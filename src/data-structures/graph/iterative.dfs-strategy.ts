ab9a607ff4f562e73579da00ca8f95f37bed1b9e
vovanphu <vovanphu1012@gmail.com>
2023-12-01 09:37:48 +0000

    Update coding convention for dfs classes

--- src/data-structures/graph/iterative.dfs-strategy.ts
+++ src/data-structures/graph/iterative.dfs-strategy.ts
@@ -4,13 +4,19 @@ export class Iterative_DfsStrategy implements DfsStrategy_Interface {
   execute(graph: Graph, startingVertex: number, callback: Function): void {
     const visited: boolean[] = new Array(graph.size()).fill(false);
     const stack: number[] = new Array<number>();
+
     stack.push(startingVertex);
+
     while (stack.length > 0) {
       const vertex: number = stack.pop() as number;
+
       if (visited[vertex]) continue;
+
       visited[vertex] = true;
       callback(vertex);
+
       const neighbors = graph.neighbors(vertex);
+      
       for (let i = neighbors.length - 1; i > -1; i--) {
         const neighbor = neighbors[i];
         stack.push(neighbor);
--- src/data-structures/graph/recursive.dfs-strategy.ts
+++ src/data-structures/graph/recursive.dfs-strategy.ts
@@ -8,9 +8,12 @@ export class Recursive_DfsStrategy implements DfsStrategy_Interface {
     visited: boolean[] = new Array(graph.size()).fill(false),
   ): void {
     if (visited[startingVertex]) return;
+
     visited[startingVertex] = true;
     callback(startingVertex);
+
     const neighbors = graph.neighbors(startingVertex);
+    
     for (const neighbor of neighbors) {
       this.execute(graph, neighbor, callback, visited);
     }
