399dd60cf6ce3c44c08bfe387d5ad2b2b17c6f6f
vovanphu <vovanphu1012@gmail.com>
2023-12-01 07:32:43 +0000

    Implement Recursive_DfsStrategy for Graph

--- src/data-structures/graph/index.ts
+++ src/data-structures/graph/index.ts
@@ -2,5 +2,6 @@ export * from './adjacency-list.graph-storage';
 export * from './adjacency-matrix.graph-storage';
 export * from './dfs-strategy.interface';
 export * from './edge-list.graph-storage';
-export * from './graph.class';
 export * from './graph-storage.inferface';
+export * from './graph.class';
+export * from './recursive.dfs-strategy';
---
+++ src/data-structures/graph/recursive.dfs-strategy.ts
@@ -0,0 +1,18 @@
+import { DfsStrategy_Interface, Graph } from '@root/data-structures';
+
+export class Recursive_DfsStrategy implements DfsStrategy_Interface {
+  execute(
+    graph: Graph,
+    startingVertex: number,
+    callback: Function,
+    visited: boolean[] = new Array(graph.size()).fill(false),
+  ): void {
+    if (visited[startingVertex]) return;
+    visited[startingVertex] = true;
+    callback(startingVertex);
+    const neighbors = graph.neighbors(startingVertex);
+    for (const neighbor of neighbors) {
+      this.execute(graph, neighbor, callback, visited);
+    }
+  }
+}
