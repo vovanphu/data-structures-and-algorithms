65f928ce938d8fcf17fb5ed830e584a04e89eb37
vovanphu <vovanphu1012@gmail.com>
2023-12-03 09:34:37 +0000

    Move grid-shortest-path to algorithms folder

--- src/algorithms/graph/bfs-grid-shortest-path.ts
+++ src/algorithms/graph/bfs-grid-shortest-path.ts
@@ -1,38 +1,54 @@
 export function bfsGridShortestPath(dungeon: string[][]): number {
-  const rr = [-1, +1, +0, +0];
-  const cc = [+0, +0, -1, +1];
   const R = dungeon.length;
   const C = dungeon[0].length;
+
+  // Define the direction vectors for top, right, down left:
+  const dr = [-1, 0, 1, 0];
+  const dc = [0, 1, 0, -1];
+
+  // Queues
   const qr: number[] = [];
   const qc: number[] = [];
+
   let stepCount = 0;
   let stepsInCurrentLayer = 1;
   let stepsInNextLayer = 0;
+
   const visited: boolean[][] = Array.from({ length: R }, () =>
     new Array(C).fill(false),
   );
+
   qr.push(0);
   qc.push(0);
   visited[0][0] = true;
+
   while (qr.length > 0) {
     const r = qr.shift() as number;
     const c = qc.shift() as number;
+
     stepsInCurrentLayer--;
+
     if (dungeon[r][c] === 'e') {
       return stepCount;
     }
-    for (let i = 0; i < rr.length; i++) {
-      const nr = r + rr[i];
-      const nc = c + cc[i];
+
+    for (let i = 0; i < dr.length; i++) {
+      const nr = r + dr[i];
+      const nc = c + dc[i];
+
       if (nr < 0 || nr >= R) continue;
       if (nc < 0 || nc >= C) continue;
       if (visited[nr][nc]) continue;
       if (dungeon[nr][nc] === 'x') continue;
+
       qr.push(nr);
       qc.push(nc);
+
       visited[nr][nc] = true;
+      
       stepsInNextLayer++;
     }
+
     if (stepsInCurrentLayer === 0) {
       stepCount++;
       stepsInCurrentLayer = stepsInNextLayer;
