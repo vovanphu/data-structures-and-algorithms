091c5e3ec6a492b37156bafa126fdd2ec55ceb09
vovanphu <vovanphu1012@gmail.com>
2024-02-22 11:08:36 +0000

    Add reconstructPath function

---
+++ src/algorithms/graph/reconstruct-path.test.ts
@@ -0,0 +1,28 @@
+import { reconstructPath } from './reconstruct-path';
+
+describe('reconstructPath', () => {
+  test('should throw error on negative vertex', () => {
+    const prev: Array<number | undefined> = [1, 3, 2, 0, 5, 4, 8, 6, 7];
+    expect(() => reconstructPath(prev, -1, 0)).toThrowError(
+      'Negative vertex is not supported',
+    );
+    expect(() => reconstructPath(prev, 5, -2)).toThrowError(
+      'Negative vertex is not supported',
+    );
+  });
+
+  test('should return empty on empty prev array', () => {
+    const prev: Array<number | undefined> = [];
+    expect(reconstructPath(prev, 0, 6)).toBeEmpty();
+  });
+
+  test('should reconstruct valid path', () => {
+    const prev: Array<number | undefined> = [undefined, 0, 1, 2, 3];
+    expect(reconstructPath(prev, 0, 3)).toEqual([0, 1, 2, 3]);
+  });
+
+  test('should return empty if could not reconstruct', () => {
+    const prev: Array<number | undefined> = [undefined, 0, 1, 2, 3];
+    expect(reconstructPath(prev, 0, 6)).toBeEmpty();
+  });
+});
---
+++ src/algorithms/graph/reconstruct-path.ts
@@ -0,0 +1,31 @@
+/**
+ * Reconstruct the path from start to end in a graph
+ * @param prev array of numbers representing the previous nodes in the path
+ * @param start the starting node
+ * @param end the ending node
+ * @returns array of numbers representing the reconstructed path,
+ * may return an empty array if the path is not reconstructable
+ */
+export function reconstructPath(
+  prev: Array<number | undefined>,
+  start: number,
+  end: number,
+): number[] {
+  if (start < 0 || end < 0) {
+    throw new Error('Negative vertex is not supported');
+  }
+
+  const reversed: number[] = [];
+  let currentNode: number | undefined = end;
+
+  while (currentNode !== undefined) {
+    reversed.push(currentNode);
+    if (currentNode === start) break;
+    currentNode = prev[currentNode];
+  }
+
+  const path = reversed.reverse();
+
+  if (path[0] === start) return path;
+  return [];
+}
