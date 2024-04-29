2aa554d7c4f379b0fad041ed839bef169b35c4de
vovanphu <vovanphu1012@gmail.com>
2024-04-29 04:59:14 +0000

    Add exports for find-components

--- src/algorithms/graph/find-components/index.ts
+++ src/algorithms/graph/find-components/index.ts
@@ -1,2 +1,4 @@
-export * from './find-components';
+export * from './bfs.find-components-strategy';
+export * from './dfs.find-components-strategy';
 export * from './find-component-strategy.interface';
+export * from './find-components';
