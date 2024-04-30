385c541bf6a84e4950fe4537eac3ed05994a8a37
vovanphu <vovanphu1012@gmail.com>
2024-04-30 08:33:33 +0000

    Update dijkstra now return prev array

--- dijkstra.test.ts
+++ dijkstra.test.ts
@@ -1,41 +1,31 @@
-import { WeightedGraph } from './src/data-structures/graph/weighted-graph.class.deprecated';
+import { Graph } from '@root/data-structures';
 import { dijkstra } from './dijkstra';
 
 describe('Dijkstra Algorithm', () => {
   test('Shortest path from start node to other nodes', () => {
-    const graph = new WeightedGraph(6);
-    graph.addEdge(0, 1, 5);
-    graph.addEdge(0, 2, 1);
-    graph.addEdge(1, 2, 2);
-    graph.addEdge(1, 3, 3);
-    graph.addEdge(1, 4, 20);
-    graph.addEdge(2, 1, 3);
-    graph.addEdge(2, 4, 12);
-    graph.addEdge(3, 2, 3);
-    graph.addEdge(3, 4, 2);
-    graph.addEdge(3, 5, 6);
-    graph.addEdge(4, 5, 1);
+    const graph = new Graph();
+    graph.set(0, 1, 5);
+    graph.set(0, 2, 1);
+    graph.set(1, 2, 2);
+    graph.set(1, 3, 3);
+    graph.set(1, 4, 20);
+    graph.set(2, 1, 3);
+    graph.set(2, 4, 12);
+    graph.set(3, 2, 3);
+    graph.set(3, 4, 2);
+    graph.set(3, 5, 6);
+    graph.set(4, 5, 1);
 
     // Calculate shortest path from node 0
-    const shortestPaths = dijkstra(graph, 0);
+    const exptectedPrev = dijkstra(graph, 0);
 
     // Verify the shortest paths from node 0 to other nodes
-    expect(shortestPaths).toEqual([0, 4, 1, 7, 9, 10]);
+    expect(exptectedPrev).toEqual([undefined, 2, 0, 1, 3, 4]);
   });
 
   test('Start index out of bounds', () => {
-    const graph = new WeightedGraph(5);
+    const graph = new Graph();
 
-    // Try to calculate shortest path with start index out of bounds
-    const shortestPaths = dijkstra(graph, 10);
-
-    // Expect all distances to be Infinity
-    expect(shortestPaths).toEqual([
-      Infinity,
-      Infinity,
-      Infinity,
-      Infinity,
-      Infinity,
-    ]);
+    expect(() => dijkstra(graph, 10)).toThrow('Out of graph bound');
   });
 });
--- dijkstra.ts
+++ dijkstra.ts
@@ -1,30 +1,46 @@
-import { WeightedGraph } from './src/data-structures/graph/weighted-graph.class.deprecated';
-import { IndexedPriorityQueue } from './src/data-structures/queue/indexed-priority-queue.class';
+import { Graph, IndexedPriorityQueue } from '@root/data-structures';
 
-export function dijkstra(graph: WeightedGraph, startIndex: number): number[] {
-  const dist: number[] = new Array(graph.size()).fill(Infinity);
-  if (startIndex >= graph.size()) return dist;
+export function dijkstra(graph: Graph, startIndex: number): number[] {
+  if (startIndex < 0 || startIndex >= graph.size()) {
+    throw Error('Out of graph bound');
+  }
+
+  // Variables used for comparing, traversing, resulting
   const visited: boolean[] = new Array(graph.size()).fill(false);
+  const dist: number[] = new Array(graph.size()).fill(Infinity);
   const priorityQueue = new IndexedPriorityQueue<number>();
+  const prev: number[] = new Array(graph.size());
+
+  // Add starting node to the queue
   dist[startIndex] = 0;
   priorityQueue.enqueue(startIndex, 0);
+
   while (priorityQueue.size() > 0) {
     const [vertex, distance] = priorityQueue.dequeue() as [number, number];
+
     if (visited[vertex]) continue;
     visited[vertex] = true;
+
+    // Ignore if we already find a better route
+    // this step may doesn't required since
+    // indexed priority queue already overwrite
+    // existing vertex when we enqueue duplicates
     if (dist[vertex] < distance) continue;
-    const neighbors = graph.neighbors(vertex);
-    for (const neighbor of neighbors) {
-      const weight = graph.weight(vertex, neighbor);
+
+    // Loop through each neighbors to relaxing the dist
+    // and add more vertices to priority queue for next traversing
+    for (const neighbor of graph.neighbors(vertex)) {
+      const weight = graph.get(vertex, neighbor);
       const oldDist = dist[neighbor];
       const newDist = dist[vertex] + weight;
+
       if (newDist < oldDist) {
         dist[neighbor] = newDist;
-      }
-      if (priorityQueue.findIndex(neighbor) === -1 || newDist < oldDist) {
         priorityQueue.enqueue(neighbor, newDist);
+        prev[neighbor] = vertex;
       }
     }
   }
-  return dist;
+
+  return prev;
 }
