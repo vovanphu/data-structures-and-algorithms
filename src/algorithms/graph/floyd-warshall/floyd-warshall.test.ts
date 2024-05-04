2c399b37a2c1b835fd2fc706ff37a32426198cba
vovanphu <vovanphu1012@gmail.com>
2024-05-04 10:28:55 +0000

    Update floyd-warshall to works and returns prevs array

--- src/algorithms/graph/floyd-warshall/floyd-warshall.test.ts
+++ src/algorithms/graph/floyd-warshall/floyd-warshall.test.ts
@@ -1,7 +1,43 @@
 import { Graph } from '@root/data-structures';
 import { floydWarshall } from './floyd-warshall';
+import { reconstructPath } from '../reconstruct-path';
 
 describe('Floyd-Warshall Algorithm', () => {
+  test('Shortest paths and cost', () => {
+    const graph = new Graph([
+      [0, 1, 1],
+      [0, 2, 4],
+      [1, 2, 2],
+      [1, 3, 3],
+      [2, 0, 1],
+      [2, 3, -1],
+      [3, 4, 2],
+    ]);
+
+    const expectedDists = [
+      [0, 1, 3, 2, 4],
+      [3, 0, 2, 1, 3],
+      [1, 2, 0, -1, 1],
+      [Infinity, Infinity, Infinity, 0, 2],
+      [Infinity, Infinity, Infinity, Infinity, 0],
+    ];
+
+    const expectedPrevs = [
+      [undefined, 0, 1, 2, 3],
+      [2, undefined, 1, 2, 3],
+      [2, 0, undefined, 2, 3],
+      [undefined, undefined, undefined, undefined, 3],
+      [undefined, undefined, undefined, undefined, undefined],
+    ];
+
+    const [dists, prevs] = floydWarshall(graph);
+
+    expect(dists).toEqual(expectedDists);
+    expect(prevs).toEqual(expectedPrevs);
+
+    expect(reconstructPath(prevs[0], 0, 4)).toEqual([0, 1, 2, 3, 4]);
+  });
+
   test('Shortest paths for a simple graph', () => {
     const graph = new Graph([
       [0, 1, 3],
--- src/algorithms/graph/floyd-warshall/floyd-warshall.ts
+++ src/algorithms/graph/floyd-warshall/floyd-warshall.ts
@@ -1,5 +1,20 @@
 import { Graph } from '@root/data-structures';
 
+/**
+ * Compute all-pairs-shortest-paths for the input graph
+ * @param graph
+ * @returns `[dists, prevs]`
+ * In this algorithm, I make some modified to return the result
+ * as prevs (array of previous node) insteads of nexts(array of next node)
+ * like the traditional algorithm, intented to use the result with
+ * function `recontructPath`
+ *
+ * For example
+ * ```ts
+ * const [dists, prevs] = floydWarshall(graph);
+ * const shortestPath03 = reconstructPath(prevs[0], 0, 3);
+ * ```
+ */
 export function floydWarshall(
   graph: Graph,
 ): [Array<Array<number>>, Array<Array<number | undefined>>] {
@@ -14,7 +29,7 @@ export function floydWarshall(
     { length: graph.size() },
     (_, i) =>
       Array.from({ length: graph.size() }, (_, j) =>
-        graph.get(i, j) === Infinity ? undefined : i,
+        i === j ? undefined : graph.get(i, j) === Infinity ? undefined : i,
       ),
   );
 
@@ -26,7 +41,7 @@ export function floydWarshall(
         const newDist = tabular[i][k] + tabular[k][j];
         if (newDist < oldDist) {
           tabular[i][j] = newDist;
-          prevs[i][j] = prevs[i][k];
+          prevs[i][j] = prevs[k][j];
         }
       }
     }
