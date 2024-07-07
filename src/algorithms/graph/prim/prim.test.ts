14f8dc4f867bf852ab97e04095dffc147cedcf22
vovanphu <vovanphu1012@gmail.com>
2024-07-07 12:31:07 +0000

    Update prim and add test cases

--- src/algorithms/graph/prim/prim.test.ts
+++ src/algorithms/graph/prim/prim.test.ts
@@ -1,4 +1,85 @@
 import { UndirectedGraph } from '@root/data-structures';
 import { prim } from './prim';
 
-describe('prim', () => {});
+describe('prim', () => {
+  test('should return the correct MST for a simple graph', () => {
+    const graph = new UndirectedGraph([
+      [0, 1, 1],
+      [0, 2, 2],
+      [1, 2, 1],
+      [1, 3, 3],
+      [2, 3, 2],
+    ]);
+
+    const result = prim(graph);
+    const expectedResult = [
+      [0, 1, 1],
+      [1, 2, 1],
+      [2, 3, 2],
+    ];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test('should return an empty array for an empty graph', () => {
+    const graph = new UndirectedGraph([]);
+
+    const mst = prim(graph);
+
+    expect(mst).toEqual([]);
+  });
+
+  test('should return an empty array for a disconnected graph', () => {
+    const graph = new UndirectedGraph([
+      [0, 1, 1],
+      [2, 3, 1],
+    ]);
+
+    const mst = prim(graph);
+
+    expect(mst).toEqual([]);
+  });
+
+  test('should return the correct MST for a more complex graph', () => {
+    const graph = new UndirectedGraph([
+      [0, 1, 10],
+      [0, 2, 6],
+      [0, 3, 5],
+      [1, 3, 15],
+      [2, 3, 4],
+    ]);
+
+    const result = prim(graph);
+
+    expect(result).toEqual(
+      expect.arrayContaining([
+        [0, 3, 5],
+        [3, 2, 4],
+        [0, 1, 10],
+      ]),
+    );
+
+    expect(result.length).toBe(graph.size() - 1);
+  });
+
+  test('should return the correct MST for a graph with equal edge weights', () => {
+    const graph = new UndirectedGraph([
+      [0, 1, 1],
+      [0, 2, 1],
+      [1, 2, 1],
+      [1, 3, 1],
+      [2, 3, 1],
+    ]);
+
+    const mst = prim(graph);
+
+    expect(mst).toEqual(
+      expect.arrayContaining([
+        [0, 1, 1],
+        [0, 2, 1],
+        [1, 3, 1],
+      ]),
+    );
+    expect(mst.length).toBe(graph.size() - 1);
+  });
+});
--- src/algorithms/graph/prim/prim.ts
+++ src/algorithms/graph/prim/prim.ts
@@ -1,6 +1,16 @@
 import { IndexedPriorityQueue, UndirectedGraph } from '@root/data-structures';
 
+/**
+ * Function to find minimum spanding tree of a weighted undirected graph by
+ * using prim's algorithm, works by utilize indexed priority queue to enable
+ * greedy approach to choose next most promissing edge of the mst
+ * @param graph
+ * @returns
+ */
 export function prim(graph: UndirectedGraph): number[][] {
+  // Base case for empty graph
+  if (graph.size() === 0) return [];
+
   /**
    * Edges of the mst
    */
@@ -16,5 +26,35 @@ export function prim(graph: UndirectedGraph): number[][] {
    */
   const priorityQueue = new IndexedPriorityQueue();
 
-  return [];
+  // Add starting point's edges to priority queue
+  for (const neighbor of graph.neighbors(0)) {
+    const weight = graph.get(0, neighbor);
+    const key = JSON.stringify([0, neighbor, weight]);
+
+    priorityQueue.enqueue(key, weight);
+  }
+
+  visited[0] = true;
+
+  while (priorityQueue.size() > 0 && edges.length !== graph.size() - 1) {
+    console.log(priorityQueue.peek());
+    const [key] = priorityQueue.dequeue() as any;
+    const [source, destination, weight]: number[] = JSON.parse(key);
+
+    if (visited[destination] === true) continue;
+
+    edges.push([source, destination, weight]);
+    visited[destination] = true;
+
+    for (const neighbor of graph.neighbors(destination)) {
+      const weight = graph.get(destination, neighbor);
+      const key = JSON.stringify([destination, neighbor, weight]);
+
+      if (visited[neighbor] === false) priorityQueue.enqueue(key, weight);
+    }
+  }
+
+  if (edges.length !== graph.size() - 1) return [];
+
+  return edges;
 }
