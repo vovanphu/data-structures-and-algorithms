16389de8daf7519f31d1079f18e94f289d6b38cf
vovanphu <vovanphu1012@gmail.com>
2024-07-07 04:57:37 +0000

    WIP: prim algorithm

--- src/algorithms/graph/prim/prim.ts
+++ src/algorithms/graph/prim/prim.ts
@@ -1,5 +1,20 @@
-import { UndirectedGraph } from '@root/data-structures';
+import { IndexedPriorityQueue, UndirectedGraph } from '@root/data-structures';
 
-export function prim(graph: UndirectedGraph): UndirectedGraph | null {
-  return null;
+export function prim(graph: UndirectedGraph): number[][] {
+  /**
+   * Edges of the mst
+   */
+  const edges: number[][] = [];
+
+  /**
+   * Variable to tracking visited
+   */
+  const visited: boolean[] = Array.from({ length: graph.size() }, () => false);
+
+  /**
+   * Indexed priority queue supports choosing the next most potential incomming edge base on weight
+   */
+  const priorityQueue = new IndexedPriorityQueue();
+
+  return [];
 }
